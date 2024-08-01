31a1f220050f9c61e66f6787d187e24bf77b003b
ygc1994 <ygc1994@naver.com>
2024-08-01 23:19:29 +0900

    chap01-literal-and-variable-level02-normal

---
+++ src/main/java/com/chap01/level02/normal/Application1.java
@@ -0,0 +1,13 @@
+package com.chap01.level02.normal;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+
+        char ch = 'a';
+
+        System.out.println("문자 a의 unicode : " + (int) ch);
+
+    }
+
+}

6de0a1e34ecc542d72ff74ef8e3607607a034f10
ygc1994 <ygc1994@naver.com>
2024-08-01 23:20:05 +0900

    chap01-literal-and-variable-level02-normal

---
+++ src/main/java/com/chap01/level02/normal/Application2.java
@@ -0,0 +1,16 @@
+package com.chap01.level02.normal;
+
+public class Application2 {
+
+    public static void main(String[] args) {
+
+        double num1 = 80.5;
+        double num2 = 50.6;
+        double num3 = 70.8;
+
+        System.out.println("총점 : " + (int) (num1 + num2 + num3));
+        System.out.println("평균 : " + ((int) (num1 + num2 + num3) / 3));
+
+    }
+
+}

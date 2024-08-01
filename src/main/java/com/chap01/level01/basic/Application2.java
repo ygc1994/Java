da0391a0369ef615b4e62b63a6b837368729ea00
ygc1994 <ygc1994@naver.com>
2024-08-01 23:18:00 +0900

    chap01-literal-and-variable-level01-basic

---
+++ src/main/java/com/chap01/level01/basic/Application2.java
@@ -0,0 +1,15 @@
+package com.chap01.level01.basic;
+
+public class Application2 {
+
+    public static void main(String[] args) {
+
+        double num1 = 12.5;
+        double num2 = 36.4;
+
+        System.out.println("면적 : " + num1 * num2);
+        System.out.println("둘레 : " + ((num1 * 2.0) + (num2 * 2.0)));
+
+    }
+
+}

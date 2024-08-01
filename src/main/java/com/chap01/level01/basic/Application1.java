2d085bd166140b194f462d7d15fee8b117c48235
ygc1994 <ygc1994@naver.com>
2024-08-01 23:17:31 +0900

    chap01-literal-and-variable-level01-basic

---
+++ src/main/java/com/chap01/level01/basic/Application1.java
@@ -0,0 +1,18 @@
+package com.chap01.level01.basic;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+
+        int num1 = 20;
+        int num2 = 30;
+
+        System.out.println("더하기 결과  : " + (num1 + num2));
+        System.out.println("빼기 결과 : " + (num1 - num2));
+        System.out.println("곱하기 결과 : " + num1 * num2);
+        System.out.println("나누기한 몫 : " + num1 / num2);
+        System.out.println("나누기한 나머지 : " + num1 % num2);
+
+    }
+
+}

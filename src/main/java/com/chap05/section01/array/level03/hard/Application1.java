0d0a71b7d0950f716f32157bf0b3f758e9dc8178
ygc1994 <ygc1994@naver.com>
2024-08-02 15:30:59 +0900

    chap05-array-level03-hard

---
+++ src/main/java/com/chap05/section01/array/level03/hard/Application1.java
@@ -0,0 +1,33 @@
+package com.chap05.section01.array.level03.hard;
+
+import java.util.Scanner;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("홀수인 양의 정수를 입력하세요 : ");
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        if (num % 2 == 0 || num < 0) {
+            System.out.println("양수 혹은 홀수만 입력해야 합니다.");
+        } else {
+            int[] arr = new int[num];
+
+            for (int i = 0; i < arr.length; i++) {
+                if (i <= arr.length / 2) {
+                    arr[i] = ++count;
+                } else arr[i] = --count;
+
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+        System.out.print("\b");
+
+    }
+
+}

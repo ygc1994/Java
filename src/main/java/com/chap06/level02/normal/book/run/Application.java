d5914276a170635fbb221e9d72cc9cf704f58ba8
ygc1994 <ygc1994@naver.com>
2024-08-05 20:00:12 +0900

    chap06-class-and-object-level02-normal

---
+++ src/main/java/com/chap06/level02/normal/book/run/Application.java
@@ -0,0 +1,19 @@
+package com.chap06.level02.normal.book.run;
+
+import com.chap06.level02.normal.book.model.dto.BookDTO;
+
+public class Application {
+    public static void main(String[] args) {
+        BookDTO bookDTO1 = new BookDTO();
+
+        bookDTO1.printInformation();
+
+        BookDTO bookDTO2 = new BookDTO("자바의 정석", "도우출판", "남궁성");
+
+        bookDTO2.printInformation();
+
+        BookDTO bookDTO3 = new BookDTO("홍길동전", "활빈당", "허균", 5000000, 0.5);
+
+        bookDTO3.printInformation();
+    }
+}

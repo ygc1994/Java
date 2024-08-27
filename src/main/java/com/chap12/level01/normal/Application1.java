e40ca0af5a2c92abb498ca0d8084519df74e5c83
ygc1994 <ygc1994@naver.com>
2024-08-27 21:38:56 +0900

    chap12-generic-제네릭 메서드 작성

---
+++ src/main/java/com/chap12/level01/normal/Application1.java
@@ -0,0 +1,26 @@
+package com.chap12.level01.normal;
+
+import java.util.Arrays;
+
+public class Application1 {
+
+    public static <T> void swap(T[] array, int index1, int index2) {
+        T temp;
+
+        temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        System.out.println("swap 전 : " + Arrays.toString(intArray));  // [1, 2, 3, 4, 5]
+        swap(intArray, 1, 3);
+        System.out.println("swap 후 : " + Arrays.toString(intArray));  // [1, 4, 3, 2, 5]
+
+        String[] strArray = {"A", "B", "C", "D"};
+        System.out.println("swap 전 : " + Arrays.toString(strArray));  // [A, B, C, D]
+        swap(strArray, 0, 2);
+        System.out.println("swap 후 : " + Arrays.toString(strArray));  // [C, B, A, D]
+    }
+}
